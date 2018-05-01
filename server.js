28f63592868862bc0f6f5f2b064d97c3e424c01a
agricardo <raranda@techlaunch.io>
2018-04-30 22:48:37 -0400

    second commit

--- server.js
+++ server.js
@@ -33,35 +33,58 @@ app.use(function (req, res, next) {
   next();
 });
 
+
+
 app.post("/", function (req, res) {
+  var emailBody = fs.readFileSync('./resume.html');
+  emailBody = emailBody;
+  var mailOptions = {
+    from: req.body.from,
+    to: req.body.dest_email,
+    html: emailBody,
+    subject: req.body.subject
+  };
+  transporter.sendMail(mailOptions, function (error, info){
+    if (error){
+      console.log(error);
+      res.end("Something went wrong. Error:\n" + error);
 
-
-
-
-  // var emailBody = fs.readFileSync('./resume.html');
-  // emailBody = emailBody;
-  // var mailOptions = {
-  //   from: req.body.from,
-  //   to: req.body.dest_email,
-  //   html: emailBody,
-  //   subject: req.body.subject
-  // };
-  // transporter.sendMail(mailOptions, function (error, info){
-  //   if (error){
-  //     console.log(error);
-  //     res.end("Something went wrong. Error:\n" + error);
-
-  //   }else{
-  //     console.log('Message sent' + info.response);
-  //     res.end("Email sent to:" + mailOptions.dest_email + ".Response:\n" + info.response);
-  //   }
-  // });
+    }else{
+      console.log('Message sent' + info.response);
+      res.end("Email sent to:" + mailOptions.dest_email + ".Response:\n" + info.response);
+    }
+  });
 });
 
 
 
 app.get('/', function (req, res){
-  res.send('Hello world')
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Send an Email</title>
+    </head>
+    <body>
+        <h1>Email Form</h1>
+        <form action="http://localhost:3934/" method="POST">
+            <div>
+                To: <input type="text" name="dest_email">
+            </div>
+            <div>
+                Subject: <input type="text" name="subject">
+            </div>
+            <div>
+                Message:: <textarea name="message" rows="10" cols="30"></textarea>
+            </div>
+            <input type="submit" value="send">
+        </form>
+    </body>
+    </html>
+  `)
 })
 
 
